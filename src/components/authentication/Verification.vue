6a9ab2fc346a44c12de648e16e5e5aaab1cff9f3
kpoptrashBlaenk <aldinmusik@gmail.com>
2025-04-24 14:36:24 +0200

    emergency push for remote

---
+++ src/components/authentication/Verification.vue
@@ -0,0 +1,29 @@
+<template>
+  <IonPage>
+    <IonContent>
+      <p class="ion-text-center italic">You are not authenticated!</p>
+      <h3 class="">Please enter your key.</h3>
+
+      <IonGrid>
+        <IonRow>
+          <IonCol size="3">
+            <div>yes</div>
+          </IonCol>
+          <IonCol size="3">
+            <div>yes</div>
+          </IonCol>
+          <IonCol size="3">
+            <div>yes</div>
+          </IonCol>
+          <IonCol size="3">
+            <div>yes</div>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
+    </IonContent>
+  </IonPage>
+</template>
+
+<script setup lang="ts">
+import { IonCol, IonContent, IonGrid, IonPage, IonRow } from '@ionic/vue'
+</script>
---
+++ src/views/VerificationPage.vue
@@ -0,0 +1,7 @@
+<template>
+  <Verification />
+</template>
+
+<script setup lang="ts">
+import Verification from '@/components/authentication/Verification.vue'
+</script>

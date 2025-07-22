ea38433386affdab9fa9d3ea800b79f212c40a32
kpoptrashBlaenk <aldinmusik@gmail.com>
2025-07-22 08:57:50 +0200

    feat(front): add ion native otp for verification page

--- src/components/authentication/VerificationPage.vue
+++ src/components/authentication/VerificationPage.vue
@@ -7,20 +7,15 @@
       <div class="mt-15 text-center text-2xl">Please enter your key.</div>
 
       <!-- Input Grid -->
-      <div class="mx-4 mt-8 grid grid-cols-6 gap-1">
-        <ion-input-otp></ion-input-otp>
-        <input
-          v-for="(otp, index) in otps"
-          class="h-15 rounded-xl border-1 border-gray-300 text-center text-3xl font-semibold transition-all duration-150 outline-none focus:border-blue-500"
-          placeholder="X"
-          :key="index"
-          v-model="otps[index]"
+      <div class="mx-4 mt-8">
+        <IonInputOtp
+          :key="otpKey"
+          :length="6"
           type="number"
-          maxlength="1"
-          @input="handleInput(index)"
-          @keydown="handleKeydown($event, index)"
-          :autofocus="index === 0"
-        />
+          size="large"
+          class="custom"
+          @ionComplete="verify($event)"
+        ></IonInputOtp>
       </div>
 
       <!-- Toast -->
@@ -33,49 +28,16 @@
 /* Import */
 import { PostConfigs, ToastComponentRef } from '@/types'
 import { apiRequestPost } from '@/utils/apiRequest'
-import { IonContent, IonPage, IonInputOtp  } from '@ionic/vue'
-import { onMounted, ref } from 'vue'
+import { IonContent, IonInputOtp, IonPage } from '@ionic/vue'
+import { ref } from 'vue'
 import ToastComponent from '../partials/ToastComponent.vue'
 
 /* Ref */
-const otps = ref<string[]>(['', '', '', '', '', ''])
-const inputs = ref<NodeListOf<HTMLInputElement>>()
 const toastRef = ref<ToastComponentRef>()
-
-/* Mounted Lifecycle Hook */
-onMounted(() => {
-  inputs.value = document.querySelectorAll('input')
-})
-
-/* DOM Manipulation */
-function handleInput(index: number): void {
-  // If input has value & not last, then focus next
-  if (
-    otps.value[index] !== undefined &&
-    otps.value[index] !== null &&
-    otps.value[index] !== '' &&
-    index < otps.value.length - 1
-  ) {
-    const nextInput = inputs.value![index + 1]
-    nextInput.focus()
-    return
-  }
-
-  // Verify
-  verify()
-}
-
-function handleKeydown(event: KeyboardEvent, index: number): void {
-  // If backspace & not first, then focus previous
-  if (event.key === 'Backspace' && !otps.value[index] && index > 0) {
-    const previousInput = inputs.value![index - 1]
-    previousInput.focus()
-    return
-  }
-}
+const otpKey = ref<number>(0)
 
 /* API Calls */
-async function verify(): Promise<void> {
+async function verify(event: CustomEvent): Promise<void> {
   const postConfigs: PostConfigs = {
     url: 'guest/auth/verify',
 
@@ -84,14 +46,14 @@ async function verify(): Promise<void> {
     },
 
     onFail: (error: Error) => {
+      otpKey.value++ // Reset OTP field
       toastRef.value?.openToast(error.message, 'error')
     },
 
-    body: () => JSON.stringify({ token: otps.value.join('') }),
+    body: () => JSON.stringify({ token: event.detail.value }),
   }
 
   // Verify token
   await apiRequestPost(postConfigs)
 }
 </script>
-npm
